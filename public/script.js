72524ea0cf2240618ad1f0b69c0b0599a0907983
PrajwalSonkusare <prajwalsonkusare01@gmail.com>
2022-02-26 15:14:09 +0530

    port

--- public/script.js
+++ public/script.js
@@ -3,8 +3,8 @@ const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
     path: '/peerjs',
     host: '/',
-    port: '443'
-    // port: '3000'
+    // port: '443'
+    port: '3000'
 })
 
 
